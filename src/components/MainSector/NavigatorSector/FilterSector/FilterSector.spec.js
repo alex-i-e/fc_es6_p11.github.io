8656f901aea98a1b516ce5def882dc696f57067b
Alex <aleksandr.i.eremin@yandex.ru>
2018-04-04 02:34:06 +0300

    [unit-tests] add tests

---
+++ src/components/MainSector/NavigatorSector/FilterSector/FilterSector.spec.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import FilterSector from "./FilterSector";
+
+import Enzyme, { configure, mount, shallow } from 'enzyme';
+// setup file
+import Adapter from 'enzyme-adapter-react-16';
+
+import 'jsdom-global/register'
+
+
+configure({ adapter: new Adapter() });
+
+
+describe('A suite <FilterSector/>', () => {
+
+  let wrapper;
+
+  it('should render without throwing an error', () => {
+    expect(shallow(<FilterSector/>).contains(
+      <div className="filter-sector">
+        <div className="filter-form"/>
+      </div>
+    )).toBe(true);
+  });
+
+  it('should render without throwing an error', () => {
+    expect(shallow(<FilterSector/>).contains(
+      <div className="filter">
+        <label htmlFor="blog-author"/>
+        <input type="text" id="blog-author"/>
+      </div>
+    )).toBe(true);
+  });
+
+  it('renders one .filter-form components', () => {
+    const wrapper = shallow(<FilterSector/>);
+    expect(wrapper.find('.filter-form').length).toBe(1);
+  });
+
+  it('should be selectable by class ".blog-author"', () => {
+    const wrapper = shallow(<FilterSector/>);
+    expect(wrapper.is('.blog-author')).toBe(true);
+  });
+
+  it('renders one <div> components', () => {
+    const wrapper = shallow(<FilterSector/>);
+    expect(wrapper.find('div').length).toBe(1);
+  });
+
+  it('should be selectable by id "blog-author"', () => {
+    expect(shallow(<FilterSector/>).is('#blog-author')).toBe(false);
+  });
+});
--- src/reducer.spec.js
+++ src/reducer.spec.js
@@ -1,4 +1,7 @@
-import { ADD_NEW_BLOG } from "./constants/actionTypes";
+import {
+  ADD_NEW_BLOG, BLOG_CREATOR_TOGGLE, FILTER_BY_AUTHOR_WAS_CHANGED,
+  HOME_PAGE_LOADED
+} from "./constants/actionTypes";
 import * as rootReducer from "enzyme";
 import configureStore from 'redux-mock-store';
 
@@ -8,14 +11,77 @@ describe('home reducers', () => {
   rootReducer.default = jest.fn();
   const mockStore = configureStore(middlewares);
 
-  it('should dispatch action', () => {
+  it('should dispatch action ADD_NEW_BLOG', () => {
     const initialState = {
       actions: {
         blogList: []
       }
     };
-    const newBlog = {};
+    const newBlog = [
+      {
+        title: 'Some title',
+        body: 'Some body',
+        date: 'Some date',
+        author: 'Some author',
+      }
+    ];
     const addNote = { type: ADD_NEW_BLOG, value: newBlog };
+
+    const store = mockStore(initialState);
+    store.dispatch(addNote);
+
+    const actions = store.getActions();
+
+    expect(actions).toEqual([addNote]);
+  });
+
+  it('should dispatch action HOME_PAGE_LOADED', () => {
+    const initialState = {
+      actions: {
+        blogList: []
+      }
+    };
+    const newBlog = {
+      title: 'Some title',
+      body: 'Some body',
+      date: 'Some date',
+      author: 'Some author',
+    };
+    const addNote = { type: HOME_PAGE_LOADED, value: newBlog };
+
+    const store = mockStore(initialState);
+    store.dispatch(addNote);
+
+    const actions = store.getActions();
+
+    expect(actions).toEqual([addNote]);
+  });
+
+  it('should dispatch action FILTER_BY_AUTHOR_WAS_CHANGED', () => {
+    const initialState = {
+      actions: {
+        filterByAuthorValue: ''
+      }
+    };
+    const filterByAuthor = 'Some new family';
+    const addNote = { type: FILTER_BY_AUTHOR_WAS_CHANGED, value: filterByAuthor };
+
+    const store = mockStore(initialState);
+    store.dispatch(addNote);
+
+    const actions = store.getActions();
+
+    expect(actions).toEqual([addNote]);
+  });
+
+  it('should dispatch action BLOG_CREATOR_TOGGLE', () => {
+    const initialState = {
+      actions: {
+        isOpenNewBlogForm: false
+      }
+    };
+    const newValue = true;
+    const addNote = { type: BLOG_CREATOR_TOGGLE, value: newValue };
 
     const store = mockStore(initialState);
     store.dispatch(addNote);
